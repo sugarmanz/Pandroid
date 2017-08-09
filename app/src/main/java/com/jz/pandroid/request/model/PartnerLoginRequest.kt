780315c03038ede2c5003470eedf8eb9f292e34d
Jeremiah Zucker <zucker.jeremiah@gmail.com>
2017-08-09 08:01:19 -0400

    WIP

--- app/src/main/java/com/jz/pandroid/request/model/PartnerLoginRequest.kt
+++ app/src/main/java/com/jz/pandroid/request/model/PartnerLoginRequest.kt
@@ -7,3 +7,13 @@ data class PartnerLoginRequest(val username: String,
                                val password: String,
                                val deviceModel: String,
                                val version: String)
+
+
+// TODO: Should probably not be
+object PartnerLogin {
+    val methodName = "auth.partnerLogin"
+    val username = "android"
+    val password = "AC7IBG09A3DTSYM4R41UJWL07VLN8JI7"
+    val deviceModel = "android-generic"
+    val version = "5"
+}
