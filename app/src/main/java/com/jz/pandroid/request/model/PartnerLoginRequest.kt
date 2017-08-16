56fa4833504817e8e5a92c2c0eecd04cae506155
Jeremiah Zucker <zucker.jeremiah@gmail.com>
2017-08-16 04:20:46 -0400

    Implement simple object serialization strategy to simplify Retrofit2 API declaration.

--- app/src/main/java/com/jz/pandroid/request/model/PartnerLoginRequest.kt
+++ app/src/main/java/com/jz/pandroid/request/model/PartnerLoginRequest.kt
@@ -1,19 +1,29 @@
 package com.jz.pandroid.request.model
 
+import com.jz.pandroid.request.MyRequestBody
+
 /**
  * Created by jzucker on 7/1/17.
  */
-data class PartnerLoginRequest(val username: String,
-                               val password: String,
-                               val deviceModel: String,
-                               val version: String)
-
-
-// TODO: Should probably not be
 object PartnerLogin {
-    val methodName = "auth.partnerLogin"
-    val username = "android"
-    val password = "AC7IBG09A3DTSYM4R41UJWL07VLN8JI7"
-    val deviceModel = "android-generic"
-    val version = "5"
+    private data class RequestBody(
+        val username: String = "android",
+        val password: String = "AC7IBG09A3DTSYM4R41UJWL07VLN8JI7",
+        val deviceModel: String = "android-generic",
+        val version: String = "5"
+    ) : MyRequestBody()
+
+    val methodName: String = "auth.partnerLogin"
+    fun getRequestBody(): MyRequestBody = RequestBody()
 }
+
+// TODO: Coming in newer versions of Kotlin (might be better alternative?)
+//class PartnerLogin {
+//    class object {
+//        val username = "android"
+//        val password = "AC7IBG09A3DTSYM4R41UJWL07VLN8JI7"
+//        val deviceModel = "android-generic"
+//        val version = "5"
+//    }
+//    val methodName = "auth.partnerLogin"
+//}
