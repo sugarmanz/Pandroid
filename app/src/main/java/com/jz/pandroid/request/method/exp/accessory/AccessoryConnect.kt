77a3c16261ab6c3e374d2a7f25e375291ad51728
Jeremiah Zucker <zucker.jeremiah@gmail.com>
2017-08-22 02:03:27 -0400

    Create test method request bodys

--- app/src/main/java/com/jz/pandroid/request/method/exp/accessory/AccessoryConnect.kt
+++ app/src/main/java/com/jz/pandroid/request/method/exp/accessory/AccessoryConnect.kt
@@ -2,7 +2,6 @@ package com.jz.pandroid.request.method.exp.accessory
 
 /**
  * Created by Jeremiah Zucker on 8/22/2017.
- * Not "unofficially" documented.
- * TODO: Determine usefulness
+ * TODO: Not "unofficially" documented. Determine usefulness.
  */
 object AccessoryConnect
--- app/src/main/java/com/jz/pandroid/request/method/exp/test/CheckLicensing.kt
+++ app/src/main/java/com/jz/pandroid/request/method/exp/test/CheckLicensing.kt
@@ -2,6 +2,9 @@ package com.jz.pandroid.request.method.exp.test
 
 /**
  * Created by Jeremiah Zucker on 8/22/2017.
+ * https://6xq.net/pandora-apidoc/json/authentication/#check-licensing
+ * TODO: If useful, may require a unique response model
  */
 object CheckLicensing {
+    val methodName: String = "test.checkLicensing"
 }
