820c6b97f3b0abfc4b0247d86c2595db611cb71a
Jeremiah Zucker <zucker.jeremiah@gmail.com>
2017-08-22 01:35:46 -0400

    Note undocumented nature of accessory method

--- app/src/main/java/com/jz/pandroid/request/method/exp/accessory/AccessoryConnect.kt
+++ app/src/main/java/com/jz/pandroid/request/method/exp/accessory/AccessoryConnect.kt
@@ -2,6 +2,7 @@ package com.jz.pandroid.request.method.exp.accessory
 
 /**
  * Created by Jeremiah Zucker on 8/22/2017.
+ * Not "unofficially" documented.
+ * TODO: Determine usefulness
  */
-object AccessoryConnect {
-}
+object AccessoryConnect
