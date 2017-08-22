f800b079ece54f358364ba0c126fc39cc625104c
Jeremiah Zucker <zucker.jeremiah@gmail.com>
2017-08-22 01:33:10 -0400

    Populated ad method objects.

--- app/src/main/java/com/jz/pandroid/request/method/exp/ad/GetAdMetadata.kt
+++ app/src/main/java/com/jz/pandroid/request/method/exp/ad/GetAdMetadata.kt
@@ -1,7 +1,18 @@
 package com.jz.pandroid.request.method.exp.ad
 
+import com.jz.pandroid.request.model.SyncTokenRequestBody
+
 /**
  * Created by Jeremiah Zucker on 8/22/2017.
+ * https://6xq.net/pandora-apidoc/json/ads/#ad-getadmetadata
  */
 object GetAdMetadata {
+    data class RequestBody(
+            val adToken: String,
+            val returnAdTrackingTokens: Boolean?,
+            val supportAudioAds: Boolean?,
+            val includeBannerAd: Boolean?
+    ) : SyncTokenRequestBody(TokenType.USER)
+
+    val methodName: String = "ad.getAdMetadata"
 }
