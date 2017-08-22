aa600c27c3e71659213aae4c8ab3151db051fe6d
Jeremiah Zucker <zucker.jeremiah@gmail.com>
2017-08-22 01:53:48 -0400

    Update auth methods to include all optional fields.

--- app/src/main/java/com/jz/pandroid/request/method/auth/PartnerLogin.kt
+++ app/src/main/java/com/jz/pandroid/request/method/auth/PartnerLogin.kt
@@ -2,13 +2,17 @@ package com.jz.pandroid.request.method.auth
 
 /**
  * Created by jzucker on 7/1/17.
+ * https://6xq.net/pandora-apidoc/json/authentication/#partner-login
  */
 object PartnerLogin {
     data class RequestBody(
             val username: String = "android",
             val password: String = "AC7IBG09A3DTSYM4R41UJWL07VLN8JI7",
             val deviceModel: String = "android-generic",
-            val version: String = "5"
+            val version: String = "5",
+            val includeUrls: Boolean? = null,
+            val returnDeviceType: Boolean? = null,
+            val returnUpdatePromptVersions: Boolean? = null
     )
 
     val methodName: String = "auth.partnerLogin"
--- app/src/main/java/com/jz/pandroid/request/method/auth/UserLogin.kt
+++ app/src/main/java/com/jz/pandroid/request/method/auth/UserLogin.kt
@@ -4,12 +4,39 @@ import com.jz.pandroid.request.model.SyncTokenRequestBody
 
 /**
  * Created by jzucker on 7/2/17.
+ * https://6xq.net/pandora-apidoc/json/authentication/#user-login
  */
 object UserLogin {
     data class RequestBody(
         val username: String,
         val password: String,
-        val loginType: String = "user"
+        val loginType: String = "user",
+        val returnGenreStations: Boolean? = null,
+        val returnCapped: Boolean? = null,
+        val includePandoraOneInfo: Boolean? = null,
+        val includeDemographics: Boolean? = null,
+        val includeAdAttributes: Boolean? = null,
+        val returnStationList: Boolean? = null,
+        val includeStationArtUrl: Boolean? = null,
+        val includeStationSeeds: Boolean? = null,
+        val includeShuffleInsteadOfQuickMix: Boolean? = null,
+        val stationArtSize: String? = null,
+        val returnCollectTrackLifetimeStats: Boolean? = null,
+        val returnIsSubscriber: Boolean? = null,
+        val xplatformAdCapable: Boolean? = null,
+        val complimentarySponsorSupported: Boolean? = null,
+        val includeSubscriptionExpiration: Boolean? = null,
+        val returnHasUsedTrial: Boolean? = null,
+        val returnUserstate: Boolean? = null,
+        val includeAccountMessage: Boolean? = null,
+        val includeUserWebname: Boolean? = null,
+        val includeListeningHours: Boolean? = null,
+        val includeFacebook: Boolean? = null,
+        val includeTwitter: Boolean? = null,
+        val includeDailySkipLimit: Boolean? = null,
+        val includeSkipDelay: Boolean? = null,
+        val includeGoogleplay: Boolean? = null,
+        val includeShowUserRecommendations: Boolean? = null
     ) : SyncTokenRequestBody(TokenType.PARTNER)
 
     val methodName: String = "auth.userLogin"
