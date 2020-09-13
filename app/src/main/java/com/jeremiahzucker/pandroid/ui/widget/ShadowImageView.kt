e1404b47bbb8e69708df9ceb8f0b48fb6997741c
Jeremiah Zucker <zucker.jeremiah@gmail.com>
2020-09-12 23:52:11 -0700

    clean up ShadowImageView

--- app/src/main/java/com/jeremiahzucker/pandroid/ui/widget/ShadowImageView.kt
+++ app/src/main/java/com/jeremiahzucker/pandroid/ui/widget/ShadowImageView.kt
@@ -2,20 +2,14 @@ package com.jeremiahzucker.pandroid.ui.widget
 
 import android.animation.ObjectAnimator
 import android.animation.ValueAnimator
-import android.annotation.TargetApi
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.RadialGradient
-import android.graphics.Shader
+import android.graphics.*
 import android.graphics.drawable.ShapeDrawable
 import android.graphics.drawable.shapes.OvalShape
 import android.os.Build
-import androidx.core.view.ViewCompat
 import android.util.AttributeSet
 import android.view.animation.LinearInterpolator
-import android.widget.ImageView
+import androidx.appcompat.widget.AppCompatImageView
 
 /**
  * Created with Android Studio.
@@ -26,7 +20,7 @@ import android.widget.ImageView
  * Stole from [android.support.v4.widget.SwipeRefreshLayout]'s implementation to display beautiful shadow
  * for circle ImageView.
  */
-class ShadowImageView : ImageView {
+class ShadowImageView : AppCompatImageView {
 
     private var mShadowRadius: Int = 0
 
@@ -34,10 +28,9 @@ class ShadowImageView : ImageView {
     private var mRotateAnimator: ObjectAnimator? = null
     private var mLastAnimationValue: Long = 0
 
-    @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : super(context, attrs, defStyleAttr)
-
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
     init {
         val density = context.resources.displayMetrics.density
@@ -49,11 +42,11 @@ class ShadowImageView : ImageView {
         val circle: ShapeDrawable
         if (elevationSupported()) {
             circle = ShapeDrawable(OvalShape())
-            ViewCompat.setElevation(this, SHADOW_ELEVATION * density)
+            elevation = SHADOW_ELEVATION * density
         } else {
             val oval = OvalShadow(mShadowRadius)
             circle = ShapeDrawable(oval)
-            ViewCompat.setLayerType(this, ViewCompat.LAYER_TYPE_SOFTWARE, circle.paint)
+            setLayerType(LAYER_TYPE_SOFTWARE, circle.paint)
             circle.paint.setShadowLayer(mShadowRadius.toFloat(), shadowXOffset.toFloat(), shadowYOffset.toFloat(), KEY_SHADOW_COLOR)
             val padding = mShadowRadius
             // set padding so the inner image sits correctly within the shadow.
@@ -71,7 +64,7 @@ class ShadowImageView : ImageView {
     }
 
     private fun elevationSupported(): Boolean {
-        return android.os.Build.VERSION.SDK_INT >= 21
+        return Build.VERSION.SDK_INT >= 21
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -112,15 +105,12 @@ class ShadowImageView : ImageView {
         }
     }
 
-    /**
-     * Draw oval shadow below ImageView under lollipop.
-     */
-    private inner class OvalShadow internal constructor(shadowRadius: Int) : OvalShape() {
+    /** Draw oval shadow below ImageView under lollipop */
+    private inner class OvalShadow(shadowRadius: Int) : OvalShape() {
         private var mRadialGradient: RadialGradient? = null
-        private val mShadowPaint: Paint
+        private val mShadowPaint: Paint = Paint()
 
         init {
-            mShadowPaint = Paint()
             mShadowRadius = shadowRadius
             updateRadialGradient(rect().width().toInt())
         }
@@ -145,16 +135,15 @@ class ShadowImageView : ImageView {
     }
 
     companion object {
+        private const val KEY_SHADOW_COLOR = 0x1E000000
+        private const val FILL_SHADOW_COLOR = 0x3D000000
 
-        private val KEY_SHADOW_COLOR = 0x1E000000
-        private val FILL_SHADOW_COLOR = 0x3D000000
+        private const val X_OFFSET = 0f
+        private const val Y_OFFSET = 1.75f
 
-        private val X_OFFSET = 0f
-        private val Y_OFFSET = 1.75f
+        private const val SHADOW_RADIUS = 24f
+        private const val SHADOW_ELEVATION = 16
 
-        private val SHADOW_RADIUS = 24f
-        private val SHADOW_ELEVATION = 16
-
-        private val DEFAULT_BACKGROUND_COLOR = 0xFF3C5F78.toInt()
+        private const val DEFAULT_BACKGROUND_COLOR = 0xFF3C5F78.toInt()
     }
 }
